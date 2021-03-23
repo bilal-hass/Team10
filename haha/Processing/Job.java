dd9d37abdb28d5122770c735a9b47572dff40142
LucasWilsonGit <55563391+LucasWilsonGit@users.noreply.github.com>
2021-03-23 21:19:08 +0000

    Added Task and Job classes early implementation
    TODO: Add unit tests
    
    Should maybe work? hm... 
    SQL set up in discord.

--- haha/Processing/Job.java
+++ haha/Processing/Job.java
@@ -1,74 +1,136 @@
 package Processing;
 
-import Controller.Date;
 import Account.CustomerAccount;
-import Payment.Payment;
+import DB.DBConnWrapper;
+import java.sql.Connection;
+import java.sql.Date;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.time.Instant;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
-public class Job {
-	private Date _urgency;
-	private int _customerNo;
-	private boolean _confirmedStatus;
-	private boolean _completedStatus;
-	private boolean _paid;
-	private int _job_ID;
-	public JobsCollection _has_multiple;
-	public SpecialInstructions _unnamed_SpecialInstructions_;
-	public CustomerAccount _belongs_to;
-	public Payment _manages;
+public class Job{
+    public static Map<Integer, String> JobTypes = new HashMap<Integer, String>();
+    static {
+        String query = "SELECT * FROM JobTypes";
+        Connection conn = DBConnWrapper.getConnection();
+        try {
+            ResultSet r = conn.createStatement().executeQuery(query);
+            while (r.next()) {
+                Integer id = r.getInt("id");
+                String JobType = r.getString("JobType");
+                JobTypes.put(id, JobType);
+            }
+        }
+        catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+    //Set up a map for ID to String for JobTypes from DB in static context so class instance not needed to map ID -> JobType
 
-	public String printLabel() {
-		throw new UnsupportedOperationException();
-	}
+    private Integer _jobId;
+    private CustomerAccount _customer;
+    private String _status;
+    private Boolean _paid;
+    private Date _startDate;
+    private Date _completionDate;
+    private Integer _jobType;
+    private ArrayList<Task> _tasks;
 
-	public Date getUrgency() {
-		return this._urgency;
-	}
+    private void UpdateSQL(String Column, Object value) {
+        String query = "UPDATE Job \n SET " + Column + " = " + value.toString() + "\n WHERE id = " + _jobId.toString() + ";";
+        Connection conn = DBConnWrapper.getConnection();
+        try {
+            Integer affected = conn.createStatement().executeUpdate(query);
+            if (affected < 1) {
+                throw new RuntimeException("Failed to update Job with ID " + _jobId.toString());
+            }
+            System.out.println("Updated Job " + _jobId.toString() + " `" + Column + "`");
+        }
+        catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
 
-	public void setUrgency(Date aUrgency) {
-		this._urgency = aUrgency;
-	}
 
-	public int getCustomerNo() {
-		return this._customerNo;
-	}
+    public Job(Integer id, CustomerAccount customer, String status, Boolean paid, Date start, Date completed, Integer type) {
+        _jobId = id;
+        _customer = customer;
+        _status = status;
+        _paid = paid;
+        _startDate = start;
+        _completionDate = completed;
+        _jobType = type;
+        _tasks = new ArrayList<Task>();
 
-	public void setCustomerNo(int aCustomerNo) {
-		this._customerNo = aCustomerNo;
-	}
+        String query = "SELECT JT.id, T.TaskType, JT.Order, JT.StartTime, JT.Completed FROM JobTasks AS JT INNER JOIN Task AS T ON T.TaskType = JT.TaskId WHERE JT.JobId = " + id.toString() + ";";
+        Connection conn = DBConnWrapper.getConnection();
+        try {
+            ResultSet RS = conn.createStatement().executeQuery(query);
+            while (RS.next()) {
+                Integer t_id = RS.getInt("id");
+                Integer t_order = RS.getInt("Order");
+                Date t_startTime = RS.getDate("StartTime");
+                Boolean t_completed = RS.getBoolean("Completed");
+                Integer t_type = RS.getInt("TaskType");
+                Task t = new Task(t_id,t_type,t_order,t_completed,t_startTime);
+                _tasks.add(t);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        //should load the Tasks automatically of the Job instance...
+    }
 
-	public boolean getConfirmedStatus() {
-		return this._confirmedStatus;
-	}
+    public Integer getId() { return _jobId; }
+    public CustomerAccount getCustomer() { return _customer; }
+    public String getStatus() { return _status; }
+    public Boolean getPaid() { return _paid; }
+    public Date getStartDate() { return _startDate; }
+    public Date getCompletionDate() { return _completionDate; }
+    public Integer getType() { return _jobType; }
+    public String getTypeString() { return JobTypes.get(_jobType); }
 
-	public void setConfirmedStatus(boolean aConfirmedStatus) {
-		this._confirmedStatus = aConfirmedStatus;
-	}
+    public void setStatus(String status) {
+        _status = status;
+        UpdateSQL("Status", status);
+    }
+    public void setPaid(Boolean paid) {
+        _paid = paid;
+        UpdateSQL("Paid", paid);
+    }
+    private void setStartDate(Date date) {
+        _startDate = date;
+        UpdateSQL("StartDate", date);
+        //needs testing
+    }
+    private void setCompletionDate(Date date) {
+        _completionDate = date;
+        UpdateSQL("CompletionDate", date);
+        //needs testing
+    }
 
-	public boolean getCompletedStatus() {
-		return this._completedStatus;
-	}
+    public void startJob() {
+        if (!getPaid()) {
+            System.out.println("Started unpaid job ID " + getId().toString() + " check Customer is Valued!");
+        }
+        setStatus("ACTIVE");
+        setStartDate((java.sql.Date)Date.from(Instant.now()));
+    }
 
-	public void setCompletedStatus(boolean aCompletedStatus) {
-		this._completedStatus = aCompletedStatus;
-	}
+    public void completeJob() {
+        setStatus("COMPLETE");
+        for (Task t: _tasks) {
+            t.completeTask();
+            //sanity complete all tasks when job completes.
+        }
+        setCompletionDate((java.sql.Date)Date.from(Instant.now()));
+    }
 
-	public boolean getPaid() {
-		return this._paid;
-	}
+    public ArrayList<Task> getTasks() {
+        return _tasks;
+    }
 
-	public void setPaid(boolean aPaid) {
-		this._paid = aPaid;
-	}
-
-	public int getJob_ID() {
-		return this._job_ID;
-	}
-
-	public void setJob_ID(int aJob_ID) {
-		this._job_ID = aJob_ID;
-	}
-
-	public Job() {
-		throw new UnsupportedOperationException();
-	}
 }
--- haha/Processing/Task.java
+++ haha/Processing/Task.java
@@ -1,58 +1,105 @@
 package Processing;
 
-import Controller.Date;
+import DB.DBConnWrapper;
+
+import java.sql.Connection;
+import java.sql.Date;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.time.Instant;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Task {
-	private TaskType _type;
-	private int _status;
-	private Date _startTime;
-	private String _staff_Name;
-	private Time _estimatedTimeLeft;
-	public SpecialInstructions _unnamed_SpecialInstructions_;
-	public TaskCollection _stores;
-	public TaskType _defines;
+    static Map<Integer, String> TaskTypeNameMap = new HashMap<Integer, String>();
+    static Map<Integer, String> TaskTypeDescriptionMap = new HashMap<Integer, String>();
+    static Map<Integer, String> TaskTypeLocationMap = new HashMap<Integer, String>();
+    static Map<Integer, Float> TaskTypePriceMap = new HashMap<Integer, Float>();
+    static Map<Integer, Integer> TaskTypeDurationMap = new HashMap<Integer, Integer>();
+    //this is kind of scuffed.... but it saves a lot of SQL calls....
 
-	public TaskType getType() {
-		return this._type;
-	}
+    static {
+        Connection conn = DBConnWrapper.getConnection();
+        String query = "SELECT TaskType as Type, TaskName as Name, TaskDescription as Description, Location, Price, Duration FROM TASK;";
+        try {
+            ResultSet RS = conn.createStatement().executeQuery(query);
+            while (RS.next()) {
+                Integer type = RS.getInt("Type");
+                String name = RS.getString("Name");
+                String description = RS.getString("Description");
+                String location = RS.getString("Location");
+                Float price = RS.getFloat("Price");
+                Integer duration = RS.getInt("Duration");
 
-	public void setType(TaskType aType) {
-		this._type = aType;
-	}
+                TaskTypeNameMap.put(type, name);
+                TaskTypeDescriptionMap.put(type, description);
+                TaskTypeLocationMap.put(type, location);
+                TaskTypePriceMap.put(type, price);
+                TaskTypeDurationMap.put(type, duration);
+            }
+        }
+        catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
 
-	public int getStatus() {
-		return this._status;
-	}
 
-	public void setStatus(int aStatus) {
-		this._status = aStatus;
-	}
+    private Integer _type;
+    private Integer _order;
+    private Integer _id;
+    private Boolean _completed;
+    private String _name;
+    private String _description;
+    private String _location;
+    private Float _price;
+    private Integer _durationMinutes;
+    private Date _startTime;
 
-	public Date getStartTime() {
-		return this._startTime;
-	}
+    public Task(Integer id, Integer type, Integer order, Boolean completed, Date startTime) {
+        _id = id;
+        _type = type;
+        _order = order;
+        _completed = completed;
+        _name = TaskTypeNameMap.get(type);
+        _description = TaskTypeDescriptionMap.get(type);
+        _location = TaskTypeLocationMap.get(type);
+        _price = TaskTypePriceMap.get(type);
+        _durationMinutes = TaskTypeDurationMap.get(type);
+        _startTime = startTime;
+    }
 
-	public void setStartTime(Date aStartTime) {
-		this._startTime = aStartTime;
-	}
+    private void UpdateJobTasksSQL(String Column, Object Value) {
+        Connection conn = DBConnWrapper.getConnection();
+        String query = "UPDATE JobTasks \n SET " + Column + " = " + Value.toString() + " \n WHERE id = " + _id.toString() + ";";
+        try {
+            Integer affected = conn.createStatement().executeUpdate(query);
+            if (affected < 1) {
+                throw new RuntimeException("Failed to update JobTask " + _id.toString() + " column " + Column);
+            }
+        }
+        catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
 
-	public String getStaff_Name() {
-		return this._staff_Name;
-	}
+    public Integer getId() { return _id; }
+    public Integer getType() { return _type; }
+    public Integer getOrder() { return _order; }
+    public Boolean getCompleted() { return _completed; }
+    public String getName() { return _name; }
+    public String getDescription() { return _description; }
+    public String getLocation() { return _location; }
+    public Float getPrice() { return _price; }
+    public Integer getDurationMinutes() { return _durationMinutes; }
+    public Date getStartTime() { return _startTime; }
 
-	public void setStaff_Name(String aStaff_Name) {
-		this._staff_Name = aStaff_Name;
-	}
+    public void startTask() {
+        _startTime = (java.sql.Date)Date.from(Instant.now());
+        UpdateJobTasksSQL("StartTime", _startTime);
+    }
 
-	public Time getEstimatedTimeLeft() {
-		return this._estimatedTimeLeft;
-	}
-
-	public void setEstimatedTimeLeft(Time aEstimatedTimeLeft) {
-		this._estimatedTimeLeft = aEstimatedTimeLeft;
-	}
-
-	public Task() {
-		throw new UnsupportedOperationException();
-	}
+    public void completeTask() {
+        _completed = true;
+        UpdateJobTasksSQL("Completed", _completed);
+    }
 }
