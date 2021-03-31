65161929117a76bb53fbacfbead12e536ad876fd
LucasWilsonGit <55563391+LucasWilsonGit@users.noreply.github.com>
2021-03-31 07:00:47 +0100

    Delete haha/GUI directory

--- haha/GUI/MyWindow.java
+++
@@ -1,8 +0,0 @@
-package GUI;
-
-public class MyWindow {
-
-	public MyWindow(int aX, int aY, int aWidth, int aHeight, ptr aBgBrush, ptr aFgBrush, ptr aIcon, String aTitle) {
-		throw new UnsupportedOperationException();
-	}
-}
